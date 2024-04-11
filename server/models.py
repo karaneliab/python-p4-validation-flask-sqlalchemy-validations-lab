e6c26200b8e6ead5ddb5545cd094954b53f34c47
karaneliab <eliabkaran7884@gmail.com>
2024-04-11 17:02:16 +0300

    done validating

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
+
 db = SQLAlchemy()
 
 class Author(db.Model):
@@ -12,7 +13,25 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
-
+    
+    @validates('name')
+    def validate_name(self,key,name):
+        if len(name) == 0:
+            raise ValueError ("Must not be empty")
+        
+        # if name unique == True:
+        #     raise ValueError("Name already exists in db")
+        return name
+        
+    @validates('phone_number')
+    def validate_phone(self, key, phone_number):
+   
+        
+        if not (phone_number.isdigit() and len(phone_number) == 10):
+            raise ValueError('Phone number must be 10 digits')
+        return phone_number
+        
+   
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -28,6 +47,28 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators  
+    @validates('content')
+    def validate_content(self,key,content):
+        if len(content) < 250:
+            raise ValueError ("Must be at least 250 characters long")
+        return content
+    @validates('summary')
+    def validate_summary(self,key, summary):
+        if len(summary) > 250:
+            raise ValueError("Summary too long test")
+        return summary
+    @validates('category')
+    def validate_category(self,key, category):
+        if category not in ['Fiction', 'Non-Fiction']:
+            raise ValueError('not avalid category')
+        return category
+    
+    @validates('title')
+    def validate_title(self, key, title):
+        clickbait_phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(phrase in title for phrase in clickbait_phrases):
+            raise ValueError("Title must contain one of the following clickbait-y phrases: 'Won't Believe', 'Secret', 'Top', 'Guess'")
+        return title
 
 
     def __repr__(self):
